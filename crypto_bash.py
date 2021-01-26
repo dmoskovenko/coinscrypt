87d1c27938ecfcc8fdaed6d80031aab9e5302a53
Dmitry <moskovenko.dmitry@yandex.ru>
2021-01-26 18:17:17 +0300

    colored output & refactoring

--- crypto_bash.py
+++ crypto_bash.py
@@ -3,12 +3,19 @@ import sys, requests, time
 # api-endpoint 
 URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false"
 
+# colored output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+GRAY='\033[1;30m'
+NC='\033[0m'
+
 def main():
 	i = 1
 	arglen = len(sys.argv)
 	escseq = '\033[' + str(arglen - 1) + 'A'
 
 	data = parsing()
+	print ('{:s}{:<12s}{:<12s}{:s}{:s}'.format(GRAY, 'Name', 'Price', '%24h', NC))
 	while i < arglen:
 		if sys.argv[i] == '-l':
 			i += 1
@@ -24,7 +31,6 @@ def main():
 			printing(URL, data, i)
 			i += 1 
 
-
 def parsing():
 	request = requests.get(url = URL) 
 	data = request.json()
@@ -34,7 +40,13 @@ def printing(url, data, i):
 	ticker = sys.argv[i]
 	for arr in data:
 		if arr['symbol'] == ticker:
-			print ('%-10s%-10s%s%%' %(arr['symbol'], arr['current_price'],  arr['price_change_percentage_24h']))
+			if arr['price_change_percentage_24h'] > 0:
+				color = GREEN
+				sign = "+"
+			else:
+				color = RED
+				sign = ""
+			print ('{:<12s}{:<12s}{:s}{:+.2f}%{:s}'.format(arr['symbol'], str(arr['current_price']), color, arr['price_change_percentage_24h'], NC))
 			break;
 
 if __name__ == "__main__":
